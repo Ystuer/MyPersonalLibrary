b029246bddcde2f91ae552c73fda142a9f6faa3f
Ystuer <185486620+Ystuer@users.noreply.github.com>
2026-04-06 16:30:30 +0200

    added splashscreen

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { globalStyles } from './src/assets/styles/globalStyle';
+import * as  SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     CustomFontRegular : require('./src/assets/fonts/Dosis-Regular.ttf'),
     CustomFontBold : require('./src/assets/fonts/Dosis-Bold.ttf'),
   })
+
+  useEffect(() => {
+    if(fontsLoaded){
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded]);
 
   if(!fontsLoaded) return null;
 

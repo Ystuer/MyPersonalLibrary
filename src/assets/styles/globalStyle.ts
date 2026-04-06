5f321c1eda4e553f1e58d10e093a9cc0f8e092b6
Ystuer <185486620+Ystuer@users.noreply.github.com>
2026-04-06 17:05:38 +0200

    added basic loginscreen/registerscreen

--- src/assets/styles/globalStyle.ts
+++ src/assets/styles/globalStyle.ts
@@ -3,9 +3,25 @@ import { StyleSheet } from 'react-native';
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDE7F6',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  box: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 20,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  imageBox: {
+    width: '80%',
+    height: 150,
+    backgroundColor: '#DDD',
+    borderRadius: 12,
+    marginBottom: 20,
   },
   text: {
     fontSize: 16,
---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,19 @@
+import { View, Text, Image } from "react-native";
+import { globalStyles } from "../assets/styles/globalStyle";
+
+export default function LoginScreen() {
+    return (
+        <View style={globalStyles.container}>
+            <View style={globalStyles.imageBox}>
+                <Image 
+                    source={require('../assets/images/MinimalistOpenBookIconCropped.png')}
+                    style={{width: '100%', height: '100%', borderRadius: 12}}
+                    resizeMode="contain"
+                /> 
+            </View>
+            <View style={globalStyles.box}>
+                <Text>Register</Text>
+            </View>
+        </View>
+    );
+}

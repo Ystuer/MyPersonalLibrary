503679697d5cf3a9aac3dac26cf756b97db0126c
Ystuer <185486620+Ystuer@users.noreply.github.com>
2026-04-06 17:27:27 +0200

    added the mockup for the dashboard screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,12 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 import { globalStyles } from "../assets/styles/globalStyle";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().min(6, 'Too short').required('Required'),
+});
 
 export default function LoginScreen() {
     return (
@@ -12,7 +19,42 @@ export default function LoginScreen() {
                 /> 
             </View>
             <View style={globalStyles.box}>
-                <Text>Login</Text>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={LoginSchema}
+                    onSubmit={(values) => console.log(values)}
+                    >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                        <View style={{ width: '100%' }}>
+                            <TextInput
+                                placeholder="Email"
+                                style={globalStyles.input}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                            {errors.email && touched.email && <Text style={globalStyles.error}>{errors.email}</Text>}
+
+                            <TextInput
+                                placeholder="Password"
+                                style={globalStyles.input}
+                                secureTextEntry
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                            />
+                            {errors.password && touched.password && <Text style={globalStyles.error}>{errors.password}</Text>}
+
+                            <TouchableOpacity onPress={() => handleSubmit()} style={globalStyles.button}>
+                                <Text style={{ color: '#fff' }}>Login</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <Text style={globalStyles.link}>Don't have an account? Register</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </Formik>
             </View>
         </View>
     );
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,7 +1,15 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 import { globalStyles } from "../assets/styles/globalStyle";
+import { Formik } from 'formik'; 
+import * as Yup from 'yup';
 
-export default function LoginScreen() {
+const RegisterSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().min(6, 'Too short').required('Required'),
+    repeatPassword: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match').required('Required'),
+});
+
+export default function RegisterScreen() {
     return (
         <View style={globalStyles.container}>
             <View style={globalStyles.imageBox}>
@@ -12,6 +20,52 @@ export default function LoginScreen() {
                 /> 
             </View>
             <View style={globalStyles.box}>
+                <Formik
+                    initialValues={{email: '', password: '', repeatPassword: ''}}
+                    validationSchema={RegisterSchema}
+                    onSubmit={(values) => console.log(values)}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                        <View style={{ width: '100%' }}>
+                            <TextInput
+                                placeholder="email"
+                                style={globalStyles.input}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                            {errors.email && touched.email && <Text style={globalStyles.error}>{errors.email}</Text>}
+                            
+                            <TextInput
+                                placeholder="Password"
+                                style={globalStyles.input}
+                                secureTextEntry
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                            />
+                            {errors.password && touched.password && <Text style={globalStyles.error}>{errors.password}</Text>}
+
+                            <TextInput
+                                placeholder="Repeat Password"
+                                style={globalStyles.input}
+                                secureTextEntry
+                                onChangeText={handleChange('repeatPassword')}
+                                onBlur={handleBlur('repeatPassword')}
+                                value={values.repeatPassword}
+                            />
+                            {errors.repeatPassword && touched.repeatPassword && (<Text style={globalStyles.error}>{errors.repeatPassword}</Text>)}
+                        
+                            <TouchableOpacity onPress={() => handleSubmit()} style={globalStyles.button}>
+                                <Text style={{ color: '#fff' }}>Register</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <Text style={globalStyles.link}>Already have an account? Login</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </Formik>
                 <Text>Register</Text>
             </View>
         </View>

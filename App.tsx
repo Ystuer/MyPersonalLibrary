d7823dc7520768fc5308ec26252d65b8f99c2824
Ystuer <185486620+Ystuer@users.noreply.github.com>
2026-04-06 16:00:41 +0200

    Initialised Project added custom font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    CustomFontRegular : require('./src/assets/fonts/Dosis-Regular.ttf'),
+    CustomFontBold : require('./src/assets/fonts/Dosis-Bold.ttf'),
+  })
+
+  if(!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontFamily: 'CustomFontBold'}}>Hello with custom font</Text>
       <StatusBar style="auto" />
     </View>
   );
